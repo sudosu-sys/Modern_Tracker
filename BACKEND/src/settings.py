d9186c59bbc093ad859c68a797b4d79a59d32e59
Sudo Su <nahomhabtamu17@gmail.com>
2026-02-08 13:12:09 +0300

    added static files configs

--- BACKEND/src/settings.py
+++ BACKEND/src/settings.py
@@ -123,7 +123,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+
 STATIC_URL = 'static/'
+
+# Directory where collectstatic will put all static files
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Allow Next.js to talk to Django
 CORS_ALLOWED_ORIGINS = [
