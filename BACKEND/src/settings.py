5bf87e5976d5f9cba26842043355c76e88d5a649
Sudo Su <nahomhabtamu17@gmail.com>
2026-08-27 11:35:28 +0300

    feat: Update ALLOWED_HOSTS to use environment variable for better configuration management

--- BACKEND/src/settings.py
+++ BACKEND/src/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['159.223.16.115', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [host.strip() for host in os.environ.get('ALLOWED_HOSTS', '').split(',') if host.strip()]
 
 
 # Application definition
